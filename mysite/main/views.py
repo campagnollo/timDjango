743979f1a03a1e720100dde56ac2667a65a56033
Eric <campagnollo2002@gmail.com>
2024-04-08 15:28:31 -0400

    working on API calls

--- mysite/main/views.py
+++ mysite/main/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.http import HttpResponseRedirect
 from .models import ToDoList, Item
 from .forms import CreateNewList
 
@@ -16,5 +17,13 @@ def home(response):
 
 
 def create(response):
-    form = CreateNewList()
+    if response.method == "POST":
+        form = CreateNewList(response.POST)
+        if form.is_valid():
+            n = form.cleaned_data["name"]
+            t = ToDoList(name=n)
+            t.save()
+        return HttpResponseRedirect("/%i" %t.id)
+    else:
+        form = CreateNewList()
     return render(response, "main/create.html", {"form": form})
