a2aff406aae69eb659a372b7af38d246098c7a44
Eric <campagnollo2002@gmail.com>
2024-04-09 11:29:11 -0400

    custom forms, work in progress

--- mysite/main/views.py
+++ mysite/main/views.py
@@ -9,6 +9,22 @@ from .forms import CreateNewList
 
 def index(response, id):
     ls = ToDoList.objects.get(id=id)
+    if response.method == 'POST':
+        print(response.POST)
+        if response.POST.get("save"):
+            for item in ls.item_set.all():
+                if response.POST.get("c" + str(item.id)) == "clicked":
+                    item.complete = True
+                else:
+                    item.complete = False
+        elif response.POST.get("newItem"):
+            txt = response.POST.get("newItem")
+            if len(txt)>2:
+                ls.item_set.create(text=txt, complete=False)
+            else:
+                print("invalid")
+                #ls.item_set.create(text=txt, complete=True)
+
     return render(response, "main/list.html", {"ls": ls})
 
 
@@ -23,7 +39,7 @@ def create(response):
             n = form.cleaned_data["name"]
             t = ToDoList(name=n)
             t.save()
-        return HttpResponseRedirect("/%i" %t.id)
+        return HttpResponseRedirect("/%i" % t.id)
     else:
         form = CreateNewList()
     return render(response, "main/create.html", {"form": form})
